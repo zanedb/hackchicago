c87f1ad05d644960635b98dad944b20b9b8645cc
Zane <github@zane.sh>
2018-06-12 16:44:39 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// .env variables
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+  if(msg.content == 'ping') {
+    msg.reply('pong');
+  }
+});
+
+// login to bot using token in .env
+client.login(process.env.TOKEN);
