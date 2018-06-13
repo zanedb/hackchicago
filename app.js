512a3a533425f392e628abbdceece2da6da806d9
Zane <github@zane.sh>
2018-06-12 18:34:44 -0700

    Add !help command

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg) => {
+  // ping pong
   if(msg.content == 'ping') {
-    msg.reply('pong');
+    msg.channel.send('pong');
   }
+
+  // help
+  if(msg.content == '!help') {
+    msg.channel.send('**Hi, I\'m Orpheus, the official Hack Chicago Dino! I can: **\n- Show you the full list of commands: `!commands`\n- Point you to <#456267567095611392> for mentor help\n- Point you to <#456267748658380812> for staff help\n- List our organizers: `!organizers`\n- Inform you of Hack Chicago rules: `!rules`')
+  }
+
+  // DEBUG
+  //console.log('message: '+msg.content)
 });
 
 // login to bot using token in .env
