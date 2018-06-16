670e1d2df5d69ee78d7e35d24c51062f5938dca3
Zane <github@zane.sh>
2018-06-16 13:38:37 -0700

    Finish converter

--- data-tools/converter.py
+++ data-tools/converter.py
@@ -1,5 +1,5 @@
 # to run: 
-# python importer/csv-json.py -i importer/attendees.csv -o importer/attendees.json -f pretty
+# python data-tools/converter.py -i data-tools/data/attendees.csv -o data-tools/data/attendees.json -f pretty
 
 # modified from https://www.idiotinside.com/2015/09/18/csv-json-pretty-print-python/
 
@@ -16,12 +16,12 @@ def main(argv):
         opts, args = getopt.getopt(argv,"hi:o:f:",["ifile=","ofile=","format="])
     except getopt.GetoptError:
         # if args are invalid, print help & exit program
-        print('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
+        print('converter.py -i <path to inputfile (CSV)> -o <path to outputfile (JSON)> -f <dump/pretty>')
         sys.exit(2)
     for opt, arg in opts:
         # if -h flag is included, print help & exit program
         if opt == '-h':
-            print('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
+            print('converter.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             input_file = arg
@@ -39,13 +39,16 @@ def read_csv(file, json_file, format):
         title = reader.fieldnames
         for row in reader:
             csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
+        for element in csv_rows:
+            # go through JSON and remove a few columns
+            element.pop('6/7 Outreach', None)
         write_json(csv_rows, json_file, format)
 
 # convert CSV data into JSON and write it
 def write_json(data, json_file, format):
     with open(json_file, "w") as f:
         if format == "pretty":
-            f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
+            f.write(json.dumps(data, sort_keys=False, indent=2, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
             print('Saved CSV data to '+json_file+' with format "'+format+'"')
         elif format == "dump":
             f.write(json.dumps(data))
