028ce5f41ec510d34d18249885ca761f799e4882
Zane <github@zane.sh>
2018-06-28 13:52:16 -0700

    Fix error (send JSON properly)

--- app.js
+++ app.js
@@ -233,7 +233,7 @@ router.route('/attendees/id/:attendee_id/approve')
         function(err, res, body) {
           if(body.status !== 200) {
             sendStat(`<@&456539994719518750>: ERROR WHILE APPROVING ATTENDEE\n\n\`\`\`${JSON.stringify(body.detail)}\`\`\``);
-            res.status(400).send({ message: body.detail });
+            res.status(400).json({ message: body.detail });
           } else {
             attendee.isApproved = true;
 
