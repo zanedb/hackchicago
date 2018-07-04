9bf0f39f046375b2df50a299d7d388e37fe0a72b
Zane <github@zane.sh>
2018-07-04 15:14:05 -0700

    a

--- app/controllers/discordBot.js
+++ app/controllers/discordBot.js
@@ -16,7 +16,7 @@ client.on('ready', async () => {
   } catch (e) {
     console.error(e)
   }
-  console.log(JSON.stringify(client.guilds));
+  console.log(client.guilds);
   //notifyStat(`<@&${discord.role.dev}>: Bot is live!`)
 })
 
