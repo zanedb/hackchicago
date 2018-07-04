32e48da9d6701ec9c6f4948015cd6935de92d594
Zane <github@zane.sh>
2018-07-03 22:16:07 -0700

    Fix indentation on commands

--- config/commands.js
+++ config/commands.js
@@ -2,62 +2,62 @@ const discord = require(`./${process.env.DISCORD_CONFIG_FILE}`)
 
 module.exports = {
   about: `
-    **Hi, I'm Orpheus. I'm Hack Club & Hack Chicago's Robot Dinosaur!** Here are a few links about me:
+**Hi, I'm Orpheus. I'm Hack Club & Hack Chicago's Robot Dinosaur!** Here are a few links about me:
 
-    - My Origin Story: https://hackclub.com/workshops/orpheus
-    - More Pictures of Me: https://github.com/hackclub/dinosaurs
-    - Hack Club (my creators): https://hackclub.com
+- My Origin Story: https://hackclub.com/workshops/orpheus
+- More Pictures of Me: https://github.com/hackclub/dinosaurs
+- Hack Club (my creators): https://hackclub.com
   `,
   commands: `
-    **Commands:**
-    - \`!about\`: Learn more about me :robot:
-    - \`!help\`: Get help from me :raised_back_of_hand:
-    - \`!commands\`: This one! :point_up_2:
-    - \`!rules\`: List the rules :straight_ruler:
-    - \`!organizers\`: List all organizers :bust_in_silhouette:
-    - \`!website\`: Learn about our website :computer:
-    - \`!social\`: Check out our social media :chart_with_upwards_trend:
+**Commands:**
+- \`!about\`: Learn more about me :robot:
+- \`!help\`: Get help from me :raised_back_of_hand:
+- \`!commands\`: This one! :point_up_2:
+- \`!rules\`: List the rules :straight_ruler:
+- \`!organizers\`: List all organizers :bust_in_silhouette:
+- \`!website\`: Learn about our website :computer:
+- \`!social\`: Check out our social media :chart_with_upwards_trend:
   `,
   help: `
-    **Hi, I'm Orpheus, the official Hack Chicago Dino! I can:**
-    - Show you the full list of commands: \`!commands\`
-    - Point you to <#${discord.channel['mentor-help']}> for mentor help
-    - Point you to <#${discord.channel['staff-help']}> for staff help
-    - List our organizers: \`!organizers\`
-    - Inform you of Hack Chicago rules: \`!rules\`
+**Hi, I'm Orpheus, the official Hack Chicago Dino! I can:**
+- Show you the full list of commands: \`!commands\`
+- Point you to <#${discord.channel['mentor-help']}> for mentor help
+- Point you to <#${discord.channel['staff-help']}> for staff help
+- List our organizers: \`!organizers\`
+- Inform you of Hack Chicago rules: \`!rules\`
   `,
   organizers: `
-    **Organizers:**
-    - Amy C.: Marketing Team
-    - Annie W.: Design Team
-    - Ava S.: Marketing Team
-    - Bhargav Y.: Finance Team
-    - Megan C.: Operations Lead
-    - Michael P.: Logistics Team
-    - Mingjie J.: Marketing Lead
-    - Musa K.: Marketing Team
-    - Sean K.: Logistics Team
-    - Victor T.: Tech Team
-    - Yev B.: Tech Team
-    - Zane D.: Tech Lead
+**Organizers:**
+- Amy C.: Marketing Team
+- Annie W.: Design Team
+- Ava S.: Marketing Team
+- Bhargav Y.: Finance Team
+- Megan C.: Operations Lead
+- Michael P.: Logistics Team
+- Mingjie J.: Marketing Lead
+- Musa K.: Marketing Team
+- Sean K.: Logistics Team
+- Victor T.: Tech Team
+- Yev B.: Tech Team
+- Zane D.: Tech Lead
 
-    **And of course, our beloved Orpheus!**
+**And of course, our beloved Orpheus!**
   `,
   rules: `
-    **Rules:**
-    You must adhere to both the Hack Club & MLH Code of Conducts.
+**Rules:**
+You must adhere to both the Hack Club & MLH Code of Conducts.
 
-    - Hack Club Code of Conduct: https://conduct.hackclub.com/
-    - MLH Code of Conduct: https://github.com/MLH/mlh-policies/blob/master/code-of-conduct.md
+- Hack Club Code of Conduct: https://conduct.hackclub.com/
+- MLH Code of Conduct: https://github.com/MLH/mlh-policies/blob/master/code-of-conduct.md
   `,
   social: `
-    **Check us out below:**
+**Check us out below:**
 
-    - Twitter: https://twitter.com/hackchicago18
-    - Instagram: https://www.instagram.com/hackchicago
-    - Facebook: https://facebook.com/hackchicago
+- Twitter: https://twitter.com/hackchicago18
+- Instagram: https://www.instagram.com/hackchicago
+- Facebook: https://facebook.com/hackchicago
 
-    **Be sure to also join our Facebook group!** https://www.facebook.com/groups/hackchicago/
+**Be sure to also join our Facebook group!** https://www.facebook.com/groups/hackchicago/
   `,
   stayawake: 'No.',
   website: 'Check out our **website** at https://hackchicago.io/.' /* Also, get **up to date alerts** for every announcement at https://hackchicago.io/live.'*/,
