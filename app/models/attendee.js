976a9b0cc94fc7df5571f10692b85792886c611a
Zane <github@zane.sh>
2018-07-20 02:56:28 -0700

    Update attendee schema

--- app/models/attendee.js
+++ app/models/attendee.js
@@ -23,7 +23,12 @@ const AttendeeSchema = new Schema({
   parentName: String,
   parentPhone: String,
   parentEmail: String,
-  role: { type: String, default: 'attendee' }
+  role: { type: String, default: 'attendee' },
+  waiverStatus: {
+    student: { type: Boolean, default: false },
+    parent: { type: Boolean, default: false }
+  },
+  checkedIn: { type: Boolean, default: false }
 })
 
 module.exports = mongoose.model('Attendee', AttendeeSchema)
